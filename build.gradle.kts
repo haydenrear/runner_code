249ce06406058436d3326f93b7532d28f999fd8e
hayden.rear <hayden.rear@gmail.com>
2024-09-22 20:03:09 -0400

    docker

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import Com_hayden_docker_gradle.DockerContext;
+
+plugins {
+    id("com.hayden.docker")
+}
+
+group = "com.hayden"
+version = "0.0.1-SNAPSHOT"
+
+tasks.register("prepareKotlinBuildScriptModel") {}
+
+
+wrapDocker {
+    ctx = arrayOf(
+        DockerContext(
+            "localhost:5001/postgres-pgvector",
+            "${project.projectDir}/src/main/docker/postgres",
+            "pgVectorPostgres"
+        ),
+        DockerContext(
+            "localhost:5001/jdk",
+            "${project.projectDir}/src/main/docker/jdk",
+            "jdk"
+        )
+    )
+}
+
+afterEvaluate {
+    tasks.getByPath("pgVectorPostgresDockerImage").dependsOn("startRegistry")
+    tasks.getByPath("pushImages").dependsOn("startRegistry")
+
+    tasks.register("startRegistry") {
+        println("Starting Registry...")
+        exec {
+            workingDir("src/main/docker")
+            commandLine("/usr/local/bin/docker-compose",  "up",  "-d")
+        }
+    }
+}
+
